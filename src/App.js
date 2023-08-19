8e5122ed5f1803cbc11c00a267d3244e8f3b45c8
Abu Hasan <Abu-Hasan1988@github.com>
2023-08-20 04:05:03 +0600

    todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import deleteSvg from './xxx.svg';
 function App() {
+
+    const [todo, setTodo] = React.useState([
+      {title:'Купить продукты', id:1},
+      {title:'Сходить на тренировку', id:2},
+      {title:'Помыть машину', id:3},
+      {title:'Помыть машину', id:4}
+    ]);
+
+    const [newTodo, setNewTodo] = React.useState('');
+    const addTodo =()=> {
+        if (newTodo.trim() !== '') {
+          setTodo([...todo,{id:Date.now(), title: newTodo}]);
+            setNewTodo('');
+        }
+    }
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        addTodo();
+      }
+    };
+    
+    const deleteToDo = (spisokId)=> {
+      setTodo(todo.filter(massiv=>massiv.id !== spisokId));
+
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <h1 className='h1'> Список to do list </h1> 
+        <div>
+        <input 
+         type='text'
+         
+         value={newTodo}
+          onChange={(event)=>setNewTodo(event.target.value)}
+          onKeyDown={handleKeyDown}
+        /> 
+         <button onClick={addTodo}>Добавить задачу</button>
+
+
+        
+        
+        
+      
+        </div>
+     <ul>
+      {todo.map(spisok => (
+      <li  className="liwka" key={spisok.id} > {spisok.title} 
+       <img 
+       className = "imagexxx"
+       src= {deleteSvg} 
+       alt='удалить'
+       onClick={()=>deleteToDo(spisok.id)}/>  
+      </li>))}
+
+
+     
+     </ul>
     </div>
   );
 }
